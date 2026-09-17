d664d4d1fcd5570f0ff01b7ae9109cbbb4199fe9
Naanzem <kurungtoenaanzem@gmail.com>
2026-09-17 03:31:37 +0100

    Fix role type errors for postgres build

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -32,7 +32,7 @@ export const authOptions: NextAuthOptions = {
           id: user.id,
           name: user.name,
           email: user.email,
-          role: user.role,
+          role: user.role as "ADMIN" | "MEMBER",
           avatar: user.avatar,
         };
       },
